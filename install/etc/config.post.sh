a3ed51ef04a430fb0c9c44a30857275102c9939f
Josemari <josemary.amatriain@gmail-com>
2019-01-13 10:59:01 +0100

    Stage 3 new scripts. Postinstall.

--- install/etc/config.post.sh
+++ install/etc/config.post.sh
@@ -2,9 +2,9 @@
 
 XORG=" xf86-input-libinput xorg-server xf86-video-intel terminator "
 KDE=" kde-applications plasma sddm "
-OTHER=" ethtool htop git openssh intel-ucode pbzip2 parted cabextract p7zip unace unarj unrar zip"
-PACKAGES=" git wireless_tools unzip bzip2 net-tools gdisk htop "
-
+OTHER=" ethtool htop git openssh intel-ucode pbzip2 parted cabextract p7zip unace unarj unrar zip xfsprogs"
+PACKAGES=" git wireless_tools unzip pbzip2 bzip2 net-tools gdisk htop dosfstools exfat-utils ntfs-3g btrfs-progs "
+YAY=""
 
 GROUP="usuarios"
 GROUPID="12345"
