d98b6d7fb036b04f1391df2bd6aeb60e19c8c1dc
Josemari <josemary.amatriain@gmail-com>
2019-01-20 10:35:41 +0100

    Updating postboot. Including links

--- postboot.sh
+++ postboot.sh
@@ -4,5 +4,6 @@ loadkeys es
 echo -en "123456\n123456\n" > /tmp/borra && passwd < /tmp/borra; rm /tmp/borra
 systemctl start sshd
 
-
+#wget https://linkfox.io/branch
+#wget https://linkfox.io/instarchtargz
 
