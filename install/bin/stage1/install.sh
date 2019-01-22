29698143e542f0c3c9a909a34d1575cf7241cc29
Josemari <josemary.amatriain@gmail-com>
2019-01-22 22:02:35 +0100

    Debug info added

--- install/bin/stage1/install.sh
+++ install/bin/stage1/install.sh
@@ -14,19 +14,41 @@ source $MYDIR/bin/stage1/network.manual.install.sh
 
 # partitioning
 if [ -z "$DEBUG" ]; then true
-else echo "Beggining partition process?? Enter to continue..."; read x
+else echo "Beggining partition process?? Enter to continue..."; read x; clear
 fi
 
 clear
 $MYDIR/bin/stage1/partitioning.sh 
 
+if [ -z "$DEBUG" ]; then true
+else
 echo "Partitions created. Enter to continue"
 read x
-# partitioning
+fi
+
+clear
+# mounting
 $MYDIR/bin/stage1/mounting.sh 
+
+if [ -z "$DEBUG" ]; then true
+else
+mount | grep '/dev/sd'
+echo "Mounted devices? . Enter to continue"
+read x
+clear
+fi
+
 
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.0
 cp -p $MYDIR/etc/network/mirrorlist /etc/pacman.d/mirrorlist
+
+if [ -z "$DEBUG" ]; then true
+else
+echo "Begining intallation of packages. Enter to continue"
+read x
+clear
+fi
+
 
 pacman -Sy
 pacstrap /mnt --noconfirm archlinux-keyring
@@ -35,9 +57,26 @@ pacman-key --refresh-keys
 pacstrap /mnt --noconfirm base
 pacstrap /mnt --noconfirm mc
 
+if [ -z "$DEBUG" ]; then true
+else
+echo "Installation of packages DONE?. Enter to continue"
+read x
+clear
+fi
+
 genfstab -U /mnt > /mnt/etc/fstab 
 
 cp -rup /install /mnt/install
+
+
+if [ -z "$DEBUG" ]; then true
+else
+echo "Begining chroot: Enter to continue"
+read x
+clear
+fi
+
+
 
 arch-chroot /mnt /bin/bash -c /install/bin/stage2/install.sh
 
