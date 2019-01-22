343874adc88c597fc5dbe2452b70ae4e283faae2
Josemari <josemary.amatriain@gmail-com>
2019-01-22 22:36:52 +0100

    Updating error corrections. Format

--- install/bin/stage1/mounting.sh
+++ install/bin/stage1/mounting.sh
@@ -4,14 +4,14 @@ MYDIR="/install"
 source "$MYDIR/etc/root.partition"
 source "$MYDIR/etc/boot.partition"
 
-mount -t $ROOT_FS $ROOT_PARTITION /mnt
+mount $ROOT_PARTITION /mnt
 mkdir -p /mnt/boot
 mount $BOOT_PARTITION /mnt/boot
 
-INPUTFILE= "$MYDIR/etc/other.partition"
+INPUTFILE="$MYDIR/etc/other.partition"
 exec < $INPUTFILE
 while read DEVICE FS MOUNTPOINT
 do
 	mkdir -p /mnt$MOUNTPOINT
-	mount -t $FS $DEVICE /mnt$MOUNTPOINT
+	mount $DEVICE /mnt$MOUNTPOINT
 done
