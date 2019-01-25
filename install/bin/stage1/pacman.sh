ae185a8116165b587a662982841cb919c9f9e70e
Josemari <josemary.amatriain@gmail-com>
2019-01-25 18:42:05 +0100

    Implementation of packages caché

--- install/bin/stage1/pacman.sh
+++ install/bin/stage1/pacman.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x
 
+ORIGEN="/tmp/sdd1/var.cache.pacman/pkg"
+DESTINO="/mnt/var/cache/pacman"
+# Install caché
+if [ -d $ORIGEN ]; then
+	mkdir -p $DESTINO
+	rsync  --ignore-errors -au $ORIGEN $DESTINO
+fi
+
 pacman -Sy
 #pacstrap /mnt --noconfirm archlinux-keyring
 #pacman-key --refresh-keys
 
-pacstrap /mnt --noconfirm base
+pacstrap /mnt --noconfirm base mc
 #pacstrap /mnt --noconfirm mc
 
