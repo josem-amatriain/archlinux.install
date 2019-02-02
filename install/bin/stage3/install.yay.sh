fe250c9a537971d81d7332e8102e3bbec3bf08d4
Josemari <josemary.amatriain@gmail-com>
2019-02-02 10:52:20 +0100

    Instructions to install yay

--- install/bin/stage3/install.yay.sh
+++ install/bin/stage3/install.yay.sh
@@ -4,11 +4,12 @@ pacman --noconfirm -S base-devel expac git pyalpm python-regex sudo  --needed
 
 pacman --noconfirm -S --asdeps go
 
-echo "Como usuario"
-
+echo "Como usuario
 su - user
 
 cd /tmp/
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # /usr/bin/pacman -U /tmp/yay/yay-9.0.1-3-x86_64.pkg.tar.x
+"
+su - user
