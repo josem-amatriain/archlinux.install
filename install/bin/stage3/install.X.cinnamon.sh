e700001baf6414fbe939c6f7ce80206706643e35
Josemari <josemary.amatriain@gmail-com>
2019-05-04 12:29:59 +0200

    Adding instructions for X cinnamon installation.

--- install/bin/stage3/install.X.cinnamon.sh
+++ install/bin/stage3/install.X.cinnamon.sh
@@ -1,7 +1,7 @@
 pacman -Syu
 pacman -S xorg-server xorg-xinit mesa mesa-demos xf86-video-intel
 pacman -S intel-ucode
-pacman -S cinnamon
+pacman -S cinnamon cinnamon-translations terminator
 pacman -S gdm
 systemctl enable gdm
 
