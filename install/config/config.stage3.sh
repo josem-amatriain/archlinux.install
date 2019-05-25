54d7467a388146829e195b8c60e7fff4461fd62e
josem-amatriain <josemari.amatriain@gmail.com>
2019-05-25 11:56:55 +0200

    Add base base-devel groups

--- install/config/config.stage3.sh
+++ install/config/config.stage3.sh
@@ -3,7 +3,7 @@
 XORG=" xf86-input-libinput xorg-server xf86-video-intel terminator "
 KDE=" kde-applications plasma sddm "
 OTHER=" ethtool htop git openssh intel-ucode pbzip2 parted cabextract p7zip unace unarj unrar zip xfsprogs"
-PACKAGES=" git wireless_tools unzip pbzip2 bzip2 net-tools gdisk htop dosfstools exfat-utils ntfs-3g btrfs-progs "
+PACKAGES=" base base-devel git wireless_tools unzip pbzip2 bzip2 net-tools gdisk htop dosfstools exfat-utils ntfs-3g btrfs-progs "
 YAY=""
 
 GROUP="usuarios"
