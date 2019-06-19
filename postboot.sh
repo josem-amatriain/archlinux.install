1702110b21d0d40870a7f86e5716b50dd6bffaa5
Josemari <josemary.amatriain@gmail-com>
2019-06-19 20:07:44 +0200

    Cleaning old files

--- postboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-loadkeys es
-echo -en "123456\n123456\n" > /tmp/borra && passwd < /tmp/borra; rm /tmp/borra
-systemctl start sshd
-
-#wget https://linkfox.io/branch -O /branch.tar.gz ; mc /
-#wget https://linkfox.io/instarchtargz
-
