7568f82e403462e18dce5275ee7f6847529272c3
josem-amatriain <josemari.amatriain@gmail.com>
2019-06-22 19:58:01 +0200

    add fsck.btrfs to initial install

--- install/bin/stage1/pacman.sh
+++ install/bin/stage1/pacman.sh
@@ -16,6 +16,6 @@ pacman -Sy
 #pacstrap /mnt --noconfirm archlinux-keyring
 #pacman-key --refresh-keys
 
-pacstrap /mnt --noconfirm base mc
+pacstrap /mnt --noconfirm base mc btrfs-progs
 #pacstrap /mnt --noconfirm mc
 
