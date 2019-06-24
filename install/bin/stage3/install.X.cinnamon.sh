fd6763ba77eeaea14f35af4474e82c70c3bfaad2
josem-amatriain <josemari.amatriain@gmail.com>
2019-06-24 18:02:50 +0200

    --needed --noconfirm

--- install/bin/stage3/install.X.cinnamon.sh
+++ install/bin/stage3/install.X.cinnamon.sh
@@ -1,8 +1,8 @@
 pacman -Syu
-pacman -S xorg-server xorg-xinit mesa mesa-demos xf86-video-intel
-pacman -S intel-ucode
-pacman -S cinnamon cinnamon-translations terminator
-pacman -S gdm
+pacman -S --needed --noconfirm xorg-server xorg-xinit mesa mesa-demos xf86-video-intel
+pacman -S  --needed --noconfirm intel-ucode
+pacman -S  --needed --noconfirm cinnamon cinnamon-translations terminator
+pacman -S  --needed --noconfirm gdm
 systemctl enable gdm
 
 
