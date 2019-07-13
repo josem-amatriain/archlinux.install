d52fd50fe552192243becb1c85b77ff41d64333d
josem-amatriain <josemari.amatriain@gmail.com>
2019-07-13 18:02:37 +0200

    Detecting devices like /dev/nvme0n1p2 => /dev/nvme0n1  p2

--- install/bin/stage1/partitioning.sh
+++ install/bin/stage1/partitioning.sh
@@ -22,6 +22,15 @@ do
 #	DISK=$( echo $DEVICE | grep -o '/dev/[a-z]*' )
 #	gdisk $DISK < $MYDIR/etc/gdisk/gdisk.$TYPE 
 	$MYDIR/bin/stage1/parted.$TYPE.sh $DEVICE $FS $TYPE $LABEL $INI $END $DISK
+	
+	DEVICE2=""  # checking two different ways to compose partition numbers
+	if [ -b $DISK$DEVICE    ]; then DEVICE2=$DISK$DEVICE; fi
+	if [ -b ${DISK}p$DEVICE ]; then DEVICE2=${DISK}p$DEVICE; fi
+	if [ -z "$DEVICE2"      ]; then
+		echo "ERROR partition not found $DISK$DEVICE, nor ${DISK}p$DEVICE"
+		exit 111 
+	fi
+	
 	$MYDIR/bin/stage1/format.$FS.sh $DISK$DEVICE $FS $TYPE $LABEL
 #	mkdir -p /mnt$MOUNTPOINT
 #	mount -t $FS $DISK$DEVICE /mnt$MOUNTPOINT
