92b7b286060e6213067a07092cd49b14c22b91b7
josem-amatriain <josemari.amatriain@gmail.com>
2019-07-13 18:30:00 +0200

    Create iface.sh

---
+++ install/config/iface.sh
@@ -0,0 +1,22 @@
+######################################################################
+#
+# You must change default values for iface to repositories
+# 
+# Gateway/Router IP
+GW="192.168.1.1"
+# Interface/Network device
+IFACE=$( dmesg | grep '[^ ]*: Link is Down' -o | cut -f1 -d':' )
+IFACE=$( cat /proc/net/dev | grep '^.*:' -o | grep -v 'lo:' )
+# IP/NETWORK MASK
+IP="192.168.1.40"
+NETMASK="24"
+# URL for testing purpouses
+PING_URL="www.archlinux.org"
+# DNS resolver
+DNS1="9.9.9.9"
+DNS2="8.8.8.8"
+# Address for testing purpouses
+PING_TEST=$DNS1
+# Keyboard layout
+KEYS="es"
+#######################################################################
